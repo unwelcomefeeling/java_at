c4e84427dfc002b5c4e4d78f037f79e968c1071c
Timur <timurkondratyev8@gmail.com>
2023-04-11 01:34:42 +0300

    variabls add

--- sandbox/src/main/java/kek/pepega/pft/MyFirstProgram.java
+++ sandbox/src/main/java/kek/pepega/pft/MyFirstProgram.java
@@ -3,7 +3,12 @@ package kek.pepega.pft;
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+		System.out.println("Hello,"+" world!");
+
+
+		int l = 125;
+		int s = l * l;
+		System.out.println("Площадь квадрата со стороной " + l + "=" +(s));
 	}
 
 }
