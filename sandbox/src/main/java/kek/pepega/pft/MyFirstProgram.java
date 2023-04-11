0e1398d314d7ba5b31c1b416f42d872e3d91b315
Timur <timurkondratyev8@gmail.com>
2023-04-11 19:32:07 +0300

    add matchematics manipulation

--- sandbox/src/main/java/kek/pepega/pft/MyFirstProgram.java
+++ sandbox/src/main/java/kek/pepega/pft/MyFirstProgram.java
@@ -3,12 +3,30 @@ package kek.pepega.pft;
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
-		System.out.println("Hello,"+" world!");
+		hello("Timik");
+		hello("Memik");
+		hello("pep");
+
+		double l = 5;
+		System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
 
 
-		int l = 125;
-		int s = l * l;
-		System.out.println("Площадь квадрата со стороной " + l + "=" +(s));
+		double a = 4;
+		double b = 6;
+		System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+	}
+
+	public static void hello(String X) {
+		System.out.println("Hello, "+X+"!");
+	}
+
+
+	public static double area (double len) {
+		return len * len;
+	}
+
+	public static double area(double a, double b) {
+		return a * b;
 	}
 
 }
